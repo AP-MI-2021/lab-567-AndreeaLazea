4e2b536476135892d5d1608bb8e079dcd2641b83
AndreeaLazea <91785714+AndreeaLazea@users.noreply.github.com>
2021-10-25 17:17:35 +0300

    Update test_domain.py

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -2,7 +2,6 @@ from domain.cheltuieli import creeaza_cheltuiala, get_alte_cheltuieli, get_suma
 from domain.cheltuieli import get_canal, get_data_an, get_data_luna, get_data_zi, get_intretinere, get_numar_apartament
 
 def test_cheltuiala():
-    cheltuiala = []
     cheltuiala = creeaza_cheltuiala(23, 200, 1, 3, 2021, 50, 30, 150)
     assert get_alte_cheltuieli(cheltuiala) == 150
     assert get_canal(cheltuiala) == 30
